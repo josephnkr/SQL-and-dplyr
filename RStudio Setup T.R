bf890c6f8ac04353a2b6512bd03a53d1ce8388b0
Joseph Rodrigo <josephnkr@gmail.com>
2017-05-30 14:35:42 -0500

    using Select in SQL and dplyr

---
+++ RStudio Setup T.R
@@ -0,0 +1,12 @@
+#installing dplyr and lahman packages
+#Also make sure the RCPP package is installed
+# remove the hash to run install scripts
+
+#install.packages("dplyr")
+library(dplyr)
+
+#install.packages("Lahman")
+library(Lahman)
+
+#install.packages("Rcpp")
+library(Rcpp)
